6c5ed9e5815871d2cafdeefa958bf569991a4135
Camryn Curtis <82342329+camrync23@users.noreply.github.com>
2024-11-21 16:59:50 -0600

    Attempt to fix pic

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  base: '/final209working/',  // Add this line to specify the base path for GitHub Pages
 });
