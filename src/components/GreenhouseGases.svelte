74080adc9b026026b9b9d2cee5112b176afe061b
Allison Conrey <110583859+allison-conrey@users.noreply.github.com>
2024-11-22 19:54:54 -0800

    Update GreenhouseGases.svelte

--- src/components/GreenhouseGases.svelte
+++ src/components/GreenhouseGases.svelte
@@ -1,7 +1,7 @@
 <section id="greenhouse-gases">
   <h2>What Are Greenhouse Gases?</h2>
   <div class="visual">
-    <img src="../../static/images/1200px-Greenhouse-effect-t2.png" alt="Illustration of the greenhouse effect" width="600">
+    <img src="static/images/1200px-Greenhouse-effect-t2.png" alt="Illustration of the greenhouse effect" width="600">
   </div>
   <p>
     Greenhouse gases (GHGs) are gases in the Earth's atmosphere that trap heat, creating a "greenhouse effect" that warms the planet. These gases, such as carbon dioxide (CO₂), methane (CH₄), and nitrous oxide (N₂O), naturally occur in the atmosphere but have increased dramatically due to human activities like burning fossil fuels, deforestation, and industrial farming. As these gases build up, they prevent some of the heat from escaping into space, leading to a gradual rise in global temperatures.
